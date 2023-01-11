fde1c86f93f85c3af87b955f9cbd59d4957dd2ae
Alex <alex_smr_91@mail.ru>
2023-01-11 13:21:04 +0400

    dz2, react frontend

---
+++ todono/frontend/src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div class="footer">
+            <ul>
+                <li><a href="#">Our Services</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+                <li><a href="#">Career</a></li>
+            </ul>
+            Copyright © 2023
+        </div>
+
+    )
+}
+
+export default Footer
---
+++ todono/frontend/src/components/menu.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Menu = () => {
+  return (
+    <nav>
+      <ul class="menuItems">
+        <li><a href='#' data-item='Home'>Home</a></li>
+        <li><a href='#' data-item='About'>About</a></li>
+        <li><a href='#' data-item='Contact'>Contact</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Menu
