67501890769226acdfba30eb131b0533b1c26863
Alex <alex_smr_91@mail.ru>
2023-02-26 17:53:25 +0400

    dz 12

--- todono/wait-for-postgres.sh
+++ todono/wait-for-postgres.sh
@@ -6,15 +6,9 @@ cmd="$@"
 
 # service/container name in the docker-compose file
 host="$1"
-# PostgreSQL port
-port="5432"
-# Database user making commection
-user="postgres"
 
-# pg_isready is a postgreSQL client tool for checking the connection
-# status of PostgreSQL server
-while ! pg_isready -h ${host} -p ${port} -U ${user} > /dev/null 2> /dev/null; do
-   echo "Connecting to postgres Failed"
+until PGPASSWORD="dante123456" psql -h "$host" -d "drf" -U "dante" -c '\q';>
+   >&2 echo "Postgres is unavailable - sleeping"
    sleep 1
 done
 
