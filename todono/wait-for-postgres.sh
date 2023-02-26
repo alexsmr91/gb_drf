00b00d3edfc6d68d626907bf240b8d2e3dc4f5fc
Alex <alex_smr_91@mail.ru>
2023-02-26 17:57:40 +0400

    dz 12

--- todono/wait-for-postgres.sh
+++ todono/wait-for-postgres.sh
@@ -7,7 +7,7 @@ cmd="$@"
 # service/container name in the docker-compose file
 host="$1"
 
-until PGPASSWORD="dante123456" psql -h "$host" -d "drf" -U "dante" -c '\q';>
+until PGPASSWORD="dante123456" psql -h "$host" -d "drf" -U "dante" -c '\q'; do
    >&2 echo "Postgres is unavailable - sleeping"
    sleep 1
 done
